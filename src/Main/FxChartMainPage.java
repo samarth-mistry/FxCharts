fff215e1eaecbf573455ec4421b1fa97b31e5e49
headathigh <samarthmistry@outlook.com>
2021-08-13 23:02:22 +0530

    Pie Chart UI improve

--- src/Main/FxChartMainPage.java
+++ src/Main/FxChartMainPage.java
@@ -26,7 +26,7 @@ public class FxChartMainPage extends Application{
 		//chartSelector(primaryStage);
 		//URL url = new File("resources/views/LineChart.fxml").toURI().toURL();
 		//Parent root =FXMLLoader.load(url);
-		setStage(primaryStage,"FxPaint");
+		setStage(primaryStage,"Pie chart");
 	}
 	private void chartSelector(Stage primaryStage) throws IOException {
 		String chrt[] = {"Pie chart", "Line chart", "Bar chart", "FxPaint"};
